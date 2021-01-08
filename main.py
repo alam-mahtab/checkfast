5f4fb061567b3f5ffb1b4135ce163d44d02056de
alam-mahtab <67095487+alam-mahtab@users.noreply.github.com>
2021-01-09 01:44:56 +0530

    origin set to *

--- main.py
+++ main.py
@@ -10,29 +10,31 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://checkfast.herokuapp.com/docs",
-    "https://checkfast.herokuapp.com/docs",
-    "uvicorn main:app --host=0.0.0.0 --port=${PORT:-5000}",
-    "uvicorn main:app --host=0.0.0.0 --port=${PORT:-5000}/docs",
-    "http://0.0.0.0:5000",
-    "http://0.0.0.0:5000/docs",
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:8000",
-    "http://localhost:4200",
-    "http://localhost:5000",
-    "http://127.0.0.1:5000",
-    "http://127.0.0.1:4200",
-    "http://127.0.0.1:8080",
-    "http://127.0.0.1:8000",
-]
+# origins = [
+#     "http://checkfast.herokuapp.com/docs",
+#     "https://checkfast.herokuapp.com/docs",
+#     "uvicorn main:app --host=0.0.0.0 --port=${PORT:-5000}",
+#     "uvicorn main:app --host=0.0.0.0 --port=${PORT:-5000}/docs",
+#     "http://0.0.0.0:5000",
+#     "http://0.0.0.0:5000/docs",
+#     "http://localhost",
+#     "http://localhost:8080",
+#     "http://localhost:8000",
+#     "http://localhost:4200",
+#     "http://localhost:5000",
+#     "http://127.0.0.1:5000",
+#     "http://127.0.0.1:4200",
+#     "http://127.0.0.1:8080",
+#     "http://127.0.0.1:8000",
+# ]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["DELETE", "GET", "POST", "PUT"],
+    allow_methods=["*"],
+    #["DELETE", "GET", "POST", "PUT"],
     allow_headers=["*"],
 )
 
