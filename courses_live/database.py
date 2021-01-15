4daf60ba0716b2ac1c3681dcd28c09bcedeed1fb
alam-mahtab <67095487+alam-mahtab@users.noreply.github.com>
2021-01-15 17:53:45 +0530

    postgresdb

--- courses_live/database.py
+++ courses_live/database.py
@@ -3,11 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./allcourse.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./allcourse.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@127.0.0.1/allcourse"
 
 some_engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args={}
 )
 SessionCourse = sessionmaker(autoflush=False ,bind=some_engine, expire_on_commit=False)
 #SessionCourse = sessionmaker(autoflush=False)
--- writer/database.py
+++ writer/database.py
@@ -3,11 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./talent&course.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./talent&course.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@127.0.0.1/talent&course"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args={}
 )
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 #SessionLocal.commit()
