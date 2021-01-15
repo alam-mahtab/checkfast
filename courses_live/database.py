2a65eb93e0bf1e79b52c4a87e7781f03ebace87f
alam-mahtab <67095487+alam-mahtab@users.noreply.github.com>
2021-01-15 18:09:34 +0530

    credentials added

--- courses_live/database.py
+++ courses_live/database.py
@@ -4,7 +4,11 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./allcourse.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@127.0.0.1/allcourse"
+#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@127.0.0.1/allcourse"
+
+# Heroku Connection
+SQLALCHEMY_DATABASE_URL = "postgresql://pabzjsrsctrfvp:4ffd0617efde5cdae2d60b2b24b9f378ae832a2bb56c2eca8b83f10697c022f7@ec2-107-23-191-123.compute-1.amazonaws.com/talent&course" 
+
 
 some_engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={}
--- writer/database.py
+++ writer/database.py
@@ -4,7 +4,12 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./talent&course.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@127.0.0.1/talent&course"
+# For local connection
+#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@127.0.0.1/talent&course" 
+
+# For Heroku connection
+SQLALCHEMY_DATABASE_URL = "postgresql://pabzjsrsctrfvp:4ffd0617efde5cdae2d60b2b24b9f378ae832a2bb56c2eca8b83f10697c022f7@ec2-107-23-191-123.compute-1.amazonaws.com/talent&course" 
+
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={}
